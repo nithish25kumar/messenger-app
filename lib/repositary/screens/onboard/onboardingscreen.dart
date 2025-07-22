1baec120b2d1918caf2a1ba98ab0015eb9222473
nithish25kumar <nithish25may2005@gmail.com>
2025-07-22 22:54:29 +0530

    screen  updated

--- lib/repositary/screens/onboard/onboardingscreen.dart
+++ lib/repositary/screens/onboard/onboardingscreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:messenger_app/domain/constants/cubits/themecubit.dart';
+import 'package:messenger_app/repositary/screens/widgets/Uihelper.dart';
 
 class Onboardingscreen extends StatelessWidget {
   const Onboardingscreen({super.key});
@@ -8,17 +7,10 @@ class Onboardingscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: Text("Themes"),
-        centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: () {
-                BlocProvider.of<Themecubit>(context).toggleTheme();
-              },
-              icon: Icon(Icons.dark_mode_outlined))
-        ],
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Uihelper.CustomImage(imgurl: "onboarding.png")],
       ),
     );
   }
---
+++ lib/repositary/screens/widgets/Uihelper.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Uihelper {
+  static CustomImage({required String imgurl}) {
+    return Image.asset("assets/images/$imgurl");
+  }
+}
