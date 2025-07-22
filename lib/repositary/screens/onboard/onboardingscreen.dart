e11673301d5883a35c8089f4e66374c2926a9587
nithish25kumar <nithish25may2005@gmail.com>
2025-07-22 23:15:02 +0530

    onboardingscreen  updated

--- lib/repositary/screens/onboard/onboardingscreen.dart
+++ lib/repositary/screens/onboard/onboardingscreen.dart
@@ -8,9 +8,30 @@ class Onboardingscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Uihelper.CustomImage(imgurl: "onboarding.png")],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Column(
+            children: [
+              Uihelper.CustomImage(
+                imgurl: "onboarding.png",
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Uihelper.CustomText(
+                  text:
+                      "Connect easily with \n your family and friends\n over countries",
+                  fontsize: 25,
+                  context: context),
+              SizedBox(
+                height: 180,
+              ),
+              Uihelper.CustomButton(
+                  buttonnname: "StartMessaging", callback: () {})
+            ],
+          ),
+        ),
       ),
     );
   }
